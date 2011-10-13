9cc4594c39824d0717b1adc9c6242996b7220893
tos1g09 <tos1g09@bc9da1e1-76ae-0410-a611-e94dc21032b9>
2011-10-13 23:23:19 +0000

    Add match deciding capability to dPeak
    git-svn-id: svn+ssh://svn.forge.ecs.soton.ac.uk/projects/sanchp3yp@18 bc9da1e1-76ae-0410-a611-e94dc21032b9

--- work/matlab/dPeak.m
+++ work/matlab/dPeak.m
@@ -3,11 +3,21 @@ function [ a, pos ] = dPeak( mat )
 
 a = 0;                      %initialise to 0
 pos = 0;
-for i = 1:length(mat)
-    if abs(mat(i)) > a
-        a = mat(i);
-        pos = i;
+average = 0;
+for i = 1:length(mat)       %scroll through array
+    average = average + abs(mat(i));    %increase average
+    if abs(mat(i)) > a      %if greater, then count it
+        a = mat(i);         %store magnitude
+        pos = i;            %store location
     end
+end
+
+%divide down average
+average = average / length(mat);
+
+if abs(a) < 0.5             %Greater than 50% match required, otherwise cancel 
+   a = 0;
+   pos = 0;
 end
 
 end
