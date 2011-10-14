efdd3ccc21c872217244bfc8d0d2b772efdc88b8
tos1g09 <tos1g09@bc9da1e1-76ae-0410-a611-e94dc21032b9>
2011-10-14 20:21:49 +0000

    Remove some unnecessary code and comments from multiPath function
    git-svn-id: svn+ssh://svn.forge.ecs.soton.ac.uk/projects/sanchp3yp@21 bc9da1e1-76ae-0410-a611-e94dc21032b9

--- work/matlab/multiPath.m
+++ work/matlab/multiPath.m
@@ -8,19 +8,12 @@ function [ out ] = multiPath( sig , chain , delay )
 %   delay:  The delay length in number of items in the array between each
 %           tap
 
-sig = [1 0.3 0.8 0.1 0.6 0 0.2];
-chain = [0.9 0.4 0.8 0.1 0.4];
 delay = 1;
-
-for i = 1:length(sig)
-    
-end
 
 out = [zeros([1 length(sig)]) zeros([1 ((length(chain)-1)*delay)])];
 for i = 1:length(chain)
     out = out + [zeros([1 (i-1)]) sig*chain(i) zeros([1 (length(out)-length(sig)-(i-1))])];;
 end
-%out = out + [zeros()][sig]];
 
 end
 
