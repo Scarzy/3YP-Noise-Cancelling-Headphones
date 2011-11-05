8b2d3b5680091d332273f9dbc6c481d9f66c9672
tos1g09 <tos1g09@bc9da1e1-76ae-0410-a611-e94dc21032b9>
2011-11-05 18:20:23 +0000

    Add interrupt service routines for reading/writing to the codec
    The routines don't have correct, or even compilable, vectors
    Also add externs to the buffers
    
    git-svn-id: svn+ssh://svn.forge.ecs.soton.ac.uk/projects/sanchp3yp@43 bc9da1e1-76ae-0410-a611-e94dc21032b9

--- work/code/codec.c
+++ work/code/codec.c
@@ -2,6 +2,20 @@
 
 DSK6713_AIC23_CodecHandle codec;
 
+volatile extern int32_t buf_l*, buf_r*;
+
+#pragma vector=YKNOW_THAT_VECTOR
+__interrupt void newSample()
+{
+	
+}
+
+#pragma vector=YKNOW_THAT_OTHER_VECTOR
+__interrupt void sendSample()
+{
+	
+}
+
 void codecSetup()
 {
 	codec = DSK6713_AIC23_openCodec(AIC23_CODEC_ID, &aic23_config);
