f9a8a940292f10a81196068b3fea2ea6b37ba280
tos1g09 <tos1g09@bc9da1e1-76ae-0410-a611-e94dc21032b9>
2011-11-05 18:20:22 +0000

    Correct the codec setup to pass the address of the config
    git-svn-id: svn+ssh://svn.forge.ecs.soton.ac.uk/projects/sanchp3yp@42 bc9da1e1-76ae-0410-a611-e94dc21032b9

--- work/code/codec.c
+++ work/code/codec.c
@@ -4,5 +4,5 @@ DSK6713_AIC23_CodecHandle codec;
 
 void codecSetup()
 {
-	codec = DSK6713_AIC23_openCodec(AIC23_CODEC_ID, aic23_config);
+	codec = DSK6713_AIC23_openCodec(AIC23_CODEC_ID, &aic23_config);
 }
