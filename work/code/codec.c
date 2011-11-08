b27aa7ccd6ec72f8dcc756b8e1b8df1ca02af9b0
tos1g09 <tos1g09@bc9da1e1-76ae-0410-a611-e94dc21032b9>
2011-11-08 19:36:34 +0000

    Add some McBSP setup stuff
    git-svn-id: svn+ssh://svn.forge.ecs.soton.ac.uk/projects/sanchp3yp@50 bc9da1e1-76ae-0410-a611-e94dc21032b9

--- work/code/codec.c
+++ work/code/codec.c
@@ -1,5 +1,7 @@
 #include "codec.h"
 
+McBSP_Handle mcbspControlHandle;
+McBSP_Handle mcbspDataHandle;
 DSK6713_AIC23_CodecHandle codec;
 
 volatile extern int32_t buf_l*, buf_r*;
@@ -13,6 +15,19 @@ __interrupt void newSample()
 #pragma vector=YKNOW_THAT_OTHER_VECTOR
 __interrupt void sendSample()
 {
+	
+}
+
+bool mcbspSetup()
+{
+	mcbspControlHandle = MCBSP_open(MCBSP_DEV0, MCBSP_OPEN_RESET);
+	mcbspDataHandle = MCBSP_open(MCBSP_DEV1, MCBSP_OPEN_RESET);
+	if(mcbspControlHandle == INV || mcbspDataHandle == INV)
+	{
+		return false;
+	}
+	MCBSP_config(mcbspControlHandle, mcbsp_control_config);
+	MCBSP_config(mcbspDataHandle, mcbsp_data_config);
 	
 }
 
