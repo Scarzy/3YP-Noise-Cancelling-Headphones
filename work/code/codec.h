c4a1ae8eb3e49309a64cccda069c64a037fb91a8
tos1g09 <tos1g09@bc9da1e1-76ae-0410-a611-e94dc21032b9>
2011-11-08 19:36:35 +0000

    Update the AIC23 codec configurations
    Including defining all 16 bits per register, rather than just 12
    
    git-svn-id: svn+ssh://svn.forge.ecs.soton.ac.uk/projects/sanchp3yp@52 bc9da1e1-76ae-0410-a611-e94dc21032b9

--- work/code/codec.h
+++ work/code/codec.h
@@ -3,16 +3,16 @@
 #define AIC23_CODEC_ID 0
 
 DSK6713_AIC23_Config aic23_config = {
-	0x017,	//Left Line In Volume, No simultaneous update - unmuted - 0dB
-	0x017,	//Right Line In Volume, No simultaneous update - unmuted - 0dB
-	0x089,	//Left Headphone Volume, No simultaneous update - zero cross - 0dB
-	0x089,	//Right Headphone Volume, No simultaneous update - zero cross - 0dB
-	0x015,	//Analog Audio Path Control, Side tone disabled - DAC on - use unmuted Mic and boost it
-	0x000,	//Digital Audio Path Control, DAC unmuted - no Deemphasis - ADC high pass enabled
-	0x040,	//Power Down Control, CLK off
-	0x001,	//Digital Audio Format, Slave - no DAC swap - 16bit input - MSB first left aligned
-	0x081,	//Sample Rate Control, ADC 48KHz - DAC 48KHz
-	0x001	//Digital Interface Activation, Active
+	0x0017,	//Left Line In Volume, No simultaneous update - unmuted - 0dB
+	0x0017,	//Right Line In Volume, No simultaneous update - unmuted - 0dB
+	0x01D9,	//Left Headphone Volume, No simultaneous update - zero cross - 0dB
+	0x01D9,	//Right Headphone Volume, No simultaneous update - zero cross - 0dB
+	0x0011,	//Analog Audio Path Control, Side tone disabled - DAC on - use unmuted Mic and boost it
+	0x0000,	//Digital Audio Path Control, DAC unmuted - no Deemphasis - ADC high pass enabled
+	0x0000,	//Power Down Control, CLK off
+	0x0043,	//Digital Audio Format, Slave - no DAC swap - 16bit input - MSB first left aligned
+	0x0081,	//Sample Rate Control, ADC 48KHz - DAC 48KHz
+	0x0001	//Digital Interface Activation, Active
 };
 
 MCBSP_Config mcbsp_data_config = {
