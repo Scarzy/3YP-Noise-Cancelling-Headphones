fb7f34585847a39e930e7899e236d183b84c8491
Thomas Scarsbrook <scarzybrook@googlemail.com>
2011-11-09 19:43:13 +0000

    Add a second ring to the ringbuffer and modify setup and other functions to suit
    Switching logic not fully implemented correctly. Currently it will switch if the
    ring the pointer is in is not the dataring, which is obviously problematic for
    the processing functions.
    Need to correct so that it only switches when the pointer is in the undesired
    ring.

--- work/code/ring.c
+++ work/code/ring.c
@@ -1,38 +1,80 @@
 #include "ring.h"
 
-void inc_ring(void * ring, int32_t ** ptr)
+void inc_ring(void * ring, int32_t ** ptr, int8_t dataring)
 {
-	if(*ptr == ((ringbuf *)ring)->ring + ((ringbuf *)ring)->size - 1)
-		*ptr = ((ringbuf *)ring)->ring;
+	if(((ringbuf *)ring)->dataring == DATA_RING)
+	{
+		if(*ptr >= ((ringbuf *)ring)->ring1)
+			*ptr = ((ringbuf *)ring)->ring0 + (*ptr - ((ringbuf *)ring)->ring1);
+		if(*ptr == ((ringbuf *)ring)->ring0 + ((ringbuf *)ring)->size - 1)
+			*ptr = ((ringbuf *)ring)->ring0;
+		else
+			*ptr = *ptr + 1;
+	}
 	else
-		*ptr = *ptr + 1;
+	{
+		if(*ptr < ((ringbuf *)ring)->ring1)
+			*ptr = ((ringbuf *)ring)->ring1 + (*ptr - ((ringbuf *)ring)->ring0);
+		if(*ptr == ((ringbuf *)ring)->ring1 + ((ringbuf *)ring)->size - 1)
+			*ptr = ((ringbuf *)ring)->ring1;
+		else
+			*ptr = *ptr + 1;
+	}
 }
 
 void dec_ring(void * ring, int32_t ** ptr)
 {
-	if(*ptr == ((ringbuf *)ring)->ring)
-		*ptr = ((ringbuf *)ring)->ring + ((ringbuf *)ring)->size - 1;
+	if(((ringbuf *)ring)->dataring == DATA_RING)
+	{
+		if(*ptr >= ((ringbuf *)ring)->ring1)
+			*ptr = ((ringbuf *)ring)->ring0 + (*ptr - ((ringbuf *)ring)->ring1);
+		if(*ptr == ((ringbuf *)ring)->ring0)
+			*ptr = ((ringbuf *)ring)->ring0 + ((ringbuf *)ring)->size - 1;
+		else
+			*ptr = *ptr - 1;
+	}
 	else
-		*ptr = *ptr - 1;
+	{
+		if(*ptr < ((ringbuf *)ring)->ring1)
+			*ptr = ((ringbuf *)ring)->ring1 + (*ptr - ((ringbuf *)ring)->ring0);
+		if(*ptr == ((ringbuf *)ring)->ring1)
+			*ptr = ((ringbuf *)ring)->ring1 + ((ringbuf *)ring)->size - 1;
+		else
+			*ptr = *ptr - 1;
+	}
 }
 
-void init_ring(void *ring, int32_t * arr, int size)
+void init_ring(void *ring, int32_t * arr0, int32_t * arr1, int size)
 {
 	int i = 0;
 	int32_t * ptr;
-	((ringbuf *)ring)->ring = arr;
+	((ringbuf *)ring)->dataring = DATA_RING;
+	((ringbuf *)ring)->ring0 = arr0;
+	((ringbuf *)ring)->ring1 = arr1;
 	((ringbuf *)ring)->size = size;
-	ptr = ((ringbuf *)ring)->ring;
+	ptr = ((ringbuf *)ring)->ring0;
 	do
 	{
 		*ptr = 0;
 		inc_ring(ring, &ptr);
 		i++;
-	}while( ptr != ((ringbuf *)ring)->ring );
+	}while( ptr != ((ringbuf *)ring)->ring0 );
+	ptr = ((ringbuf *)ring)->ring1;
+	do
+	{
+		*ptr = 0;
+		inc_ring(ring, &ptr);
+		i++;
+	}while( ptr != ((ringbuf *)ring)->ring1 );
 }
 
-void init_ring_ptr(void *ring, int32_t ** ptr)
+void init_ring_ptr(void *ring, int32_t ** ptr, int8_t dataring)
 {
-	*ptr = ((ringbuf *)ring)->ring;
+	*ptr = (dataring == DATA_RING) ? ((ringbuf *)ring)->ring0 : ((ringbuf *)ring)->ring1;
 }
- 
+
+void switch_ring(void * ring)
+{
+	((ringbuf *)ring)->dataring = 1 - ((ringbuf *)ring)->dataring;
+}
+
--- work/code/ring.h
+++ work/code/ring.h
@@ -4,9 +4,14 @@
 #include <stdint.h>
 #include <tistdtypes.h>
 
+#define DATA_RING 0
+#define PROC_RING 1
+
 typedef struct {
-	int32_t * ring;
+	int32_t * ring0;
+	int32_t * ring1;
 	int32_t size;
+	int8_t dataring;
 } ringbuf;
 
 void init_ring(void *ring, int32_t * arr, int size);
@@ -14,5 +19,7 @@ void init_ring_ptr(void *ring, int32_t ** ptr);
 
 void inc_ring(void * ring, int32_t ** ptr);
 void dec_ring(void * ring, int32_t ** ptr);
+
+void switch_ring(void * ring);
 
 #endif /*RING_H_*/
