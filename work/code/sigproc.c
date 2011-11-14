23ab150a600121771f74234e592c6272764fbdd0
tos1g09 <tos1g09@bc9da1e1-76ae-0410-a611-e94dc21032b9>
2011-11-14 01:22:50 +0000

    Convert the Matlab cross-correlation function into C
    git-svn-id: svn+ssh://svn.forge.ecs.soton.ac.uk/projects/sanchp3yp@86 bc9da1e1-76ae-0410-a611-e94dc21032b9

---
+++ work/code/sigproc.c
@@ -0,0 +1,41 @@
+
+void crosscorr(int16_t * arr1, int16_t * arr2, double * res, int length, int maxdel)
+{
+	double mean1, mean2;
+	mean1 = mean(arr1, length);
+	mean2 = mean(arr2, length);
+	
+	for(int del = -maxdel; i < maxdel; i++)
+	{
+		int num = 0;
+		for(int i = 0; i < length; i++)
+		{
+			int j = i + del;
+			if(!( (j < 0) || (j >= length) ))
+			{
+				num += ((arr1[i] - mean1) * (arr2[j] - mean2));
+			}
+		}
+		
+		int sum1 = 0;
+		int sum2 = 0;
+		for(int i = 0; i < length; i++)
+		{
+			sum1 += ((arr1[i] - mean1) * (arr1[i] - mean1));
+			sum2 += ((arr2[i] - mean2) * (arr2[i] - mean2));
+		}
+		double s = sqrt(sum1 * sum2);
+		res[del + maxdel] = num / s;
+	}
+}
+
+double mean(int16_t * arr, int length)
+{
+	double mean = 0;
+	for(int i = 0; i < length; i++)
+	{
+		mean += arr[i];
+	}
+	return mean / length;
+}
+
--- work/code/sigproc.h
+++ work/code/sigproc.h
@@ -1,6 +1,9 @@
 #ifndef __SIG_PROC_H_
 #define __SIG_PROC_H_
 
+#include <math.h>
 
+void crosscorr(int16_t * arr1, int16_t * arr2, double * res, int length, int maxdel);
+double mean(int16_t * arr, int length);
 
 #endif /* __SIG_PROC_H_ */
