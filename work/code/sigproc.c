64eee8bca392ffcca675c9c20218ca966b329aed
tos1g09 <tos1g09@bc9da1e1-76ae-0410-a611-e94dc21032b9>
2011-11-15 02:00:47 +0000

    Re-arrange sigproc code to make it compatible with CCS C89 restrictions
    C99 standardised in May 2000, it's 11.5 years old and still TI haven't adopted
    it...
    
    git-svn-id: svn+ssh://svn.forge.ecs.soton.ac.uk/projects/sanchp3yp@100 bc9da1e1-76ae-0410-a611-e94dc21032b9

--- work/code/sigproc.c
+++ work/code/sigproc.c
@@ -1,20 +1,21 @@
 #include "sigproc.h"
 
-void cancel(void * ring1, int16_t * ptr1in, void * ring2, int16_t * ptr2in, void * res, int16_t * ptrres)
+void cancel(void * ring1, int32_t * ptr1in, void * ring2, int32_t * ptr2in, void * res, int32_t * ptrres)
 {
-	double cc[((ringbuf *)ring1)->size];
-	int16_t * ptr1 = ptr1in;
-	int16_t * ptr2 = ptr2in;
+	int shift,i;
+	double cc[2*CROSS_CORR_MAX_DELAY+1];
+	int32_t * ptr1 = ptr1in;
+	int32_t * ptr2 = ptr2in;
 	crosscorr(ring1, ptr1, ring2, ptr2, cc, ((ringbuf *)ring1)->size, CROSS_CORR_MAX_DELAY);
-	int shift = dpeak(cc, ((ringbuf *)ring1)->size);
+	shift = dpeak(cc, ((ringbuf *)ring1)->size);
 	if(shift < 0)
 	{
-		for(int i = 0; i < shift; i++)
+		for(i = 0; i < shift; i++)
 		{
 			inc_ring(&ring2, &ptr2);
 		}
 	}
-	for(int i = 0; i < ((ringbuf *)ring1)->size; i++)
+	for(i = 0; i < ((ringbuf *)ring1)->size; i++)
 	{
 		*ptrres = ((i - shift) <= 0) ? *ptr1 : *ptr1 - *ptr2 ;
 		inc_ring(&ring1, &ptr1);
@@ -23,22 +24,25 @@ void cancel(void * ring1, int16_t * ptr1in, void * ring2, int16_t * ptr2in, void
 	}
 }
 
-void crosscorr(void * ring1, int16_t * ptr1in, void * ring2, int16_t * ptr2in, double * res, int length, int maxdel)
+void crosscorr(void * ring1, int32_t * ptr1in, void * ring2, int32_t * ptr2in, double * res, int length, int maxdel)
 {
-	int16_t * ptr1 = ptr1in;
-	int16_t * ptr2 = ptr2in;
+	int del, i, j;
 	double mean1, mean2;
+	int32_t * ptr1 = ptr1in;
+	int32_t * ptr2 = ptr2in;
 	mean1 = mean(ring1, ptr1);
 	mean2 = mean(ring2, ptr2);
 	
-	for(int del = -maxdel; i < maxdel; i++)
+	for(del = -maxdel; del < maxdel; del++)
 	{
+		int sum1, sum2;
+		double s;
 		int num = 0;
 		ptr1 = ptr1in;
 		ptr2 = ptr2in;
-		for(int i = 0; i < length; i++)
+		for(i = 0; i < length; i++)
 		{
-			int j = i + del;
+			j = i + del;
 			if(!( (j < 0) || (j >= length) ))
 			{
 				num += ((*ptr1 - mean1) * (*ptr2 - mean2));
@@ -49,16 +53,16 @@ void crosscorr(void * ring1, int16_t * ptr1in, void * ring2, int16_t * ptr2in, d
 		
 		ptr1 = ptr1in;
 		ptr2 = ptr2in;
-		int sum1 = 0;
-		int sum2 = 0;
-		for(int i = 0; i < length; i++)
+		sum1 = 0;
+		sum2 = 0;
+		for(i = 0; i < length; i++)
 		{
 			sum1 += ((*ptr1 - mean1) * (*ptr1 - mean1));
 			sum2 += ((*ptr2 - mean2) * (*ptr2 - mean2));
 			inc_ring(&ring1, &ptr1);
 			inc_ring(&ring2, &ptr2);
 		}
-		double s = sqrt(sum1 * sum2);
+		s = sqrt(sum1 * sum2);
 		res[del + maxdel] = num / s;
 	}
 }
@@ -66,8 +70,8 @@ void crosscorr(void * ring1, int16_t * ptr1in, void * ring2, int16_t * ptr2in, d
 int dpeak(double * arr, int length)
 {
 	double max = 0;
-	int pos = 0;
-	for(int i = 0; i < length; i++)
+	int i, pos = 0;
+	for(i = 0; i < length; i++)
 	{
 		if(abs(arr[i]) > max)
 		{
@@ -80,9 +84,10 @@ int dpeak(double * arr, int length)
 
 double mean(void * ring, int32_t * ptrin)
 {
-	int32_t * ptr = ptrin;
+	int i;
 	double mean = 0;
-	for(int i = 0; i < ((ringbuf *)ring)->size; i++)
+	int32_t * ptr = ptrin;
+	for(i = 0; i < ((ringbuf *)ring)->size; i++)
 	{
 		mean += *ptr;
 		inc_ring(&ring, &ptr);
--- work/code/sigproc.h
+++ work/code/sigproc.h
@@ -6,8 +6,8 @@
 
 #define CROSS_CORR_MAX_DELAY 200
 
-void cancel(void * ring1, int16_t * ptr1in, void * ring2, int16_t *ptr2in, void * res, int16_t * ptrres);
-void crosscorr(void * ring1, int16_t * ptr1in, void * ring2, int16_t * ptr2in, double * res, int length, int maxdel);
+void cancel(void * ring1, int32_t * ptr1in, void * ring2, int32_t *ptr2in, void * res, int32_t * ptrres);
+void crosscorr(void * ring1, int32_t * ptr1in, void * ring2, int32_t * ptr2in, double * res, int length, int maxdel);
 int dpeak(double * arr, int length);
 double mean(void * ring, int32_t * ptrin);
 
