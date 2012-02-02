392a23852b30d28bff746f42a13da39a01d68fe8
Thomas Scarsbrook <scarzybrook@googlemail.com>
2012-02-02 10:34:38 +0000

    Construct the function to apply a tap delay line

--- work/code/lms.c
+++ work/code/lms.c
@@ -5,7 +5,16 @@ void build_tap()
 	
 }
 
-void apply_tap()
+void apply_tap(void * ring, int16_t * ptrin, double * tap, int tap_length, int16_t * out)
 {
-	
+	int16_t * ptr = ptrin;
+	int16_t sum = 0;
+	int i;
+	for(i = tap_length - 1; i >= 0; i--)
+	{
+		sum += tap[i] * *ptr;
+		dec_ring(ring, &ptr);
+	}
+	*out = sum;
 }
+
--- work/code/lms.h
+++ work/code/lms.h
@@ -2,6 +2,6 @@
 #define __LMS_H_
 
 void build_tap();
-void apply_tap();
+void apply_tap(void * ring, int16_t * ptrin, double * tap, int tap_length, int16_t * out);
 
 #endif	/* __LMS_H_ */
