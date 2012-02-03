a40fa48efabd77a946df956ce73ad278c5d58cca
Thomas Scarsbrook <scarzybrook@googlemail.com>
2012-02-03 20:36:07 +0000

    Add an adaption step size as a #define

--- work/code/lms.h
+++ work/code/lms.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+#define MU_VALUE 0.8
+
 void build_tap(double * cc, int ccsize, double * ac, int acsize, double * tap, int taplength);
 void apply_tap(void * ring, int16_t * ptrin, double * tap, int tap_length, int16_t * out);
 void gen_error(int16_t * desin, int16_t * estin, int16_t * err);
