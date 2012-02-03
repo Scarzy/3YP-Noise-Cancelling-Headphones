546d5a45eaddb8f449e1b27d1726b3744a40ca5c
Thomas Scarsbrook <scarzybrook@googlemail.com>
2012-02-03 21:26:25 +0000

    Add some comments to the sigproc code

--- work/code/sigproc.c
+++ work/code/sigproc.c
@@ -31,7 +31,7 @@ void crosscorr(void * ring1, int16_t * ptr1in, void * ring2, int16_t * ptr2in, d
 {
 	int del, i, j;
 	double mean1, mean2;
-	int16_t * ptr1 = ptr1in;
+	int16_t * ptr1 = ptr1in;				//Set the pointers for use to the input value
 	int16_t * ptr2 = ptr2in;
 	double s, sqrttmp0, sqrttmp1;
 	double sum1 = 0, sum2 = 0;
@@ -39,53 +39,53 @@ void crosscorr(void * ring1, int16_t * ptr1in, void * ring2, int16_t * ptr2in, d
 	int pos = 0;
 	double max = 0;
 	
-	ccmeansum1 += *ptr1;
-	inc_ring(ring1, &ptr1);
-	ccmeansum1 -= *ptr1;
+	ccmeansum1 += *ptr1;					//Add the latest value to the meansum
+	inc_ring(ring1, &ptr1);					//Increment the ring, now pointing at the final value, ie. the oldest one that gets lost
+	ccmeansum1 -= *ptr1;					//Subtract this value
 	
-	ccmeansum2 += *ptr2;
-	inc_ring(ring2, &ptr2);
-	ccmeansum2 -= *ptr2;
+	ccmeansum2 += *ptr2;					//Add the latest value to the meansum
+	inc_ring(ring2, &ptr2);					//Increment the ring, now pointing at the final value, ie. the oldest one that gets lost
+	ccmeansum2 -= *ptr2;					//Subtract this value
 	
-	mean1 = ccmeansum1 / ((ringbuf *)ring1)->size;
-	mean2 = ccmeansum2 / ((ringbuf *)ring2)->size;
+	mean1 = ccmeansum1 / ((ringbuf *)ring1)->size;		//Divide the meansum down to get the mean
+	mean2 = ccmeansum2 / ((ringbuf *)ring2)->size;		//Divide the meansum down to get the mean
 	
-	for(i = 0; i < length; i++)
+	for(i = 0; i < length; i++)				//Loop for the number of resultant values, generating normalising factor
 	{
-		sum1 += ((*ptr1 - mean1) * (*ptr1 - mean1));
-		sum2 += ((*ptr2 - mean2) * (*ptr2 - mean2));
-		inc_ring(ring1, &ptr1);
+		sum1 += ((*ptr1 - mean1) * (*ptr1 - mean1));	//Add the latest summation value
+		sum2 += ((*ptr2 - mean2) * (*ptr2 - mean2));	
+		inc_ring(ring1, &ptr1);				//Increment round the rings
 		inc_ring(ring2, &ptr2);
 	}
 	
-	sqrttmp0 = sum1 * sum2;
-	sqrttmp1 = fabs(sqrttmp0);
-	s = sqrt(sqrttmp1);
+	sqrttmp0 = sum1 * sum2;					//Multiply the two summations together
+	sqrttmp1 = fabs(sqrttmp0);				//Acquire the absolute value of the multiplication (sanity checking)
+	s = sqrt(sqrttmp1);					//Squareroot the value, s is now the normalising factor
 	
-	for(del = -maxdel; del <= maxdel; del++)
+	for(del = -maxdel; del <= maxdel; del++)		//Loop for all required delays
 	{
 		double num = 0;
-		ptr1 = ptr1in;
+		ptr1 = ptr1in;					//Reset the ring pointer
 		ptr2 = ptr2in;
-		for(i = 0; i < length; i++)
+		for(i = 0; i < length; i++)			//Loop for length of buffer
 		{
-			j = i + del;
-			if(!( (j < 0) || (j >= length) ))
+			j = i + del;				//Calculate position of j
+			if(!( (j < 0) || (j >= length) ))	//If j is in usable bounds
 			{
-				num += ((*ptr1 - mean1) * (*ptr2 - mean2));
+				num += ((*ptr1 - mean1) * (*ptr2 - mean2));	//Add up the summation
 			}
-			inc_ring(ring1, &ptr1);
+			inc_ring(ring1, &ptr1);			//Increment round the rings
 			inc_ring(ring2, &ptr2);
 		}
 			
-		res[del + maxdel] = num / s;
+		res[del + maxdel] = num / s;			//Assign the normalised value
 		
-		if(abs(res[del + maxdel]) > max)				
+		if(abs(res[del + maxdel]) > max)		//Detect if a new peak
 		{
-			max = abs(res[del + maxdel]);
-			pos = (del + maxdel);
+			max = abs(res[del + maxdel]);		//Get height of peak
+			pos = (del + maxdel);			//Get position of peak
 		}
 	}
-	shift = pos;
+	shift = pos;						//Set the peak value
 }
 
