6f171c8bccecd7cb9ff232b81d496caa9690c448
Thomas Scarsbrook <scarzybrook@googlemail.com>
2012-05-01 17:32:16 +0100

    Fix the LMS code
    Includes adding a init_taps function

--- work/code/lms.c
+++ work/code/lms.c
@@ -1,12 +1,21 @@
 #include "lms.h"
 
+void init_taps(double * taps, int taplength)
+{
+	int i;
+	for(i = 0; i < taplength; i++)
+	{
+		taps[i] = 0;
+	}
+}
+
 void build_tap(void * ring, int16_t * ptrin, double * err, double * tap, int taplength, double * norm)
 {
 	int16_t * ptr = ptrin;
 	int i;
 	for(i = taplength - 1; i >= 0; i--)
 	{
-		tap[i] = LEAK_FACTOR * tap[i] + (MU_VALUE * noiin[i] * *err / *norm);
+		tap[i] = LEAK_FACTOR * tap[i] + (MU_VALUE * *ptr * *err / *norm);
 		dec_ring(ring, &ptr);
 	}
 }
@@ -26,10 +35,10 @@ void apply_tap(void * ring, int16_t * ptrin, double * tap, int tap_length, int16
 
 void gen_error(int16_t * desin, int16_t * estin, double * err)
 {
-	*err = *desin - *errin;
+	*err = *desin - *estin;
 }
 
-void calc_norm(void ring, int16_t * ptrin, double * normout)
+void calc_norm(void * ring, int16_t * ptrin, int tap_length, double * normout)
 {
 	int16_t * ptr = ptrin;
 	double sum = 0;
