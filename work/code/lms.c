8cf828b9fa7601c2f5b760626998e1f6e21660bb
Thomas Scarsbrook <scarzybrook@googlemail.com>
2012-05-02 05:10:41 +0100

    Reduce number of memory accesses

--- work/code/lms.c
+++ work/code/lms.c
@@ -12,18 +12,19 @@ void init_taps(double * taps, int taplength)
 void apply_tap(void * ring, int16_t * ptrin, double * tap, int tap_length, int16_t * out, double * normout, int16_t * desin, double * err)
 {
 	int16_t * ptr = ptrin;
-	int16_t sum = 0;
-	double normsum;
+	int16_t sum = 0, val;
+	double normsum = NORM_MIN;
 	int i;
 	for(i = tap_length - 1; i >= 0; i--)
 	{
-		tap[i] = LEAK_FACTOR * tap[i] + (MU_VALUE * *ptr * *err / *normout);
-		sum += tap[i] * *ptr;
-		normsum += (*ptr * *ptr);
+		val = *ptr;
+		tap[i] = LEAK_FACTOR * tap[i] + (MU_VALUE * val * *err / *normout);
+		sum += tap[i] * val;
+		normsum += (val * val);
 		dec_ring(ring, &ptr);
 	}
 	*out = sum;
 	*normout = normsum;
-	*err = *desin - *out;
+	*err = *desin - sum;
 }
 
